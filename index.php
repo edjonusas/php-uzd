9eb7a170ace8467c95ce516aa02798f83c01f1a9
Edwis0513 <ed.jonusas@gmail.com>
2020-08-18 13:08:28 +0300

    bamba

--- index.php
+++ index.php
@@ -5,14 +5,35 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php print 'As, ' . date('l') . ' ir php'; ?></title>
+    <title>bomb</title>
+    <style>
+
+        main>div {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .bomb {
+            border-radius: 50%;
+            background: cadetblue;
+            width: <?php print date('s')*10; ?>px;
+            height: <?php print date('s')*10; ?>px;
+        }
+
+        body {
+            font-size: 30px;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        Edvinas - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('Y') . ' metu'; ?>
-    </h1>
-    <p>
-        Viskas prasidejo <?php print date('F') . ' menesio ' . date('d') . " diena!"; ?>
-    </p>
+<main>
+    <div>
+        <div><span class="time"><?php print date('s'); ?></span></div>
+        <div class="bomb"></div>
+    </div>
+</main>
 </body>
 </html>
