15729e69cd45afa5de1680c4e9c8a3c5b8a1c159
Edwis0513 <ed.jonusas@gmail.com>
2020-08-18 10:48:05 +0300

    taisyta

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>As valdau HTML</title>
+    <title>As valdau HTML ir<?php print 'PHP'; ?></title>
 </head>
 <body>
-    <h1>Edvinas</h1>
+
+    <h1>Edvinas - HTL ir <?php print 'PHP'; ?> asas </h1>
     <p>Moku kurti statini turini.</p>
     <p>Atidaryta <?php print date('l jS \of F Y h:i:s A'); ?></p>
 </body>
