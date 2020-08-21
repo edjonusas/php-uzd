fcf839a0e8473f6cb39992d4220d8230253a066b
Edwis0513 <ed.jonusas@gmail.com>
2020-08-21 15:23:32 +0300

    game

--- index.php
+++ index.php
@@ -1,34 +1,61 @@
 <?php
 
-define('G', ['r' => 0, 'g' => 128, 'b' => 0]);   //Green led
-define('R', ['r' => 255, 'g' => 0, 'b' => 0]);   //Red led
-define('Y', ['r' => 255, 'g' => 255, 'b' => 0]); //Yellow led
-
-$array = [
-    [
-        G, G, G, G, G, G, G, G
-    ],
-    [
-        G, Y, Y, Y, Y, Y, Y, G
-    ],
-    [
-        G, Y, R, R, R, R, Y, G
-    ],
-    [
-        G, Y, R, Y, Y, R, Y, G
-    ],
-    [
-        G, Y, R, Y, Y, R, Y, G
-    ],
-    [
-        G, Y, R, R, R, R, Y, G
-    ],
-    [
-        G, Y, Y, Y, Y, Y, Y, G
-    ],
-    [
-        G, G, G, G, G, G, G, G
-    ],
+$game = [
+    'objects' => [
+        [
+            'x' => 50,
+            'y' => 80,
+            'class' => 'car'
+        ],
+        [
+            'x' => 23,
+            'y' => 80,
+            'class' => 'ballas'
+        ],
+        [
+            'x' => 40,
+            'y' => 80,
+            'class' => 'cop-car'
+        ],
+        [
+            'x' => 60,
+            'y' => 78,
+            'class' => 'vagos'
+        ],
+    ]
 ];
 
-var_dump($array);
+https://i.pinimg.com/originals/03/c9/de/03c9de444b1f392ed18e67ea0d7d7aac.png
+
+foreach ($game['objects'] as $key => $object) {
+    $game['objects'][$key]['on_fire'] = rand(0, 1) ? 'fire' : '';
+    if ($game['objects'][$key]['on_fire'] === 'fire') {
+        $game['objects'][$key]['is_target'] = '';
+    } else {
+        $game['objects'][$key]['is_target'] = 'target';
+    }
+}
+var_dump($game);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<div class="bg">
+    <?php foreach ($game['objects'] as $object): ?>
+        <div class="<?= $object['class'] . " " . $object['on_fire'] . " " . $object['is_target'] ?>"
+             style=" left:<?= $object['x'] ?>% ; top: <?= $object['y'] ?>vh"></div>
+    <?php endforeach; ?>
+</div>
+</body>
+</html>
