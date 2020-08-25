cc5583730aa606ab4b6c782708d745086bf15733
Edwis0513 <ed.jonusas@gmail.com>
2020-08-25 15:35:23 +0300

    uzduotys zilvinas

--- index.php
+++ index.php
@@ -1,123 +1,237 @@
 <?php
-
-$game = [
-    'objects' => [
+$vilnius = [
+    "mayor" => 'Remigijus Šimašius',
+    "number" => '+370 35 654 84',
+    "area" => 401,
+    "elderships" => [
         [
-            'x' => 50,
-            'y' => 80,
-            'class' => 'car'
-        ],
-        [
-            'x' => 23,
-            'y' => 80,
-            'class' => 'ballas'
+            "title" => "Antakalnio seniūnija",
+            "address" => "Antakalnio g. 17, 10312 Vilnius",
+            "elder" => "Taurintas Rudys",
+            "population" => 52369
         ],
         [
-            'x' => 40,
-            'y' => 80,
-            'class' => 'cop-car'
+            "title" => "Fabijoniškių",
+            "address" => "S. Stanevičiaus g. 24, LT-07102 Vilnius",
+            "elder" => "Jonas Novikevičius",
+            "population" => 22369
         ],
         [
-            'x' => 60,
-            'y' => 78,
-            'class' => 'vagos'
+            "title" => "Grigiškių",
+            "address" => "Vilniaus g. 6, LT-27101 Grigiškės, Vilniaus m. sav.",
+            "elder" => "Leonard Klimovič",
+            "population" => 32111
         ],
-    ],
-    'hud' => [
-        'time' => date('H:i', strtotime("3 hours")),
-        'armor' => rand(1, 100),
-        'health' => rand(1, 100),
-        'weapon' => [
-            [
-                'name' => 'breaker',
-                'ammo' => 'infinite'
-            ],
-            [
-                'name' => 'katana',
-                'ammo' => 'infinite'
-            ],
-            [
-                'name' => 'knife',
-                'ammo' => 'infinite'
-            ],
-            [
-                'name' => 'm4',
-                'ammo' => '120'
-            ],
+        [
+            "title" => "Naujininkų Seniūnija",
+            "address" => "Dariaus ir Girėno g. 11, LT-02170 Vilnius",
+            "elder" => "Raimondas Lingys",
+            "population" => 37001
         ],
-        'money' => 100,
-        'wanted' => 0,
+        [
+            "title" => "Senamiesčio seniūnija",
+            "address" => "Odminių g. 3, LT-01122 Vilnius",
+            "elder" => "Irena Paukštytė",
+            "population" => 19423
+        ],
     ]
-];
-
-$fire_count = 0;
-
-foreach ($game['objects'] as $key => $object) {
-
-    $object['on_fire'] = rand(true, false);
-    $object['on_fire'] ? ++$fire_count : 0;
-
-    $object['is_target'] = !$object['on_fire'];
-    $object['class'] .= ' ' . ($object['on_fire'] ? 'fire' : 'target');
-
-    $game['objects'][$key] = $object;
-}
-
-//money
-$money_count = 13000 + 520 * $fire_count;
-$length = 8; //digits
-$game['hud']['money'] = '$' . substr(str_repeat(0, $length) . $money_count, -$length);
-
-//wanted level
-$game['hud']['wanted'] = $fire_count;
-
-
-$active_weapon = $game['hud']['weapon'][rand(0, count($game['hud']['weapon']) - 1)]['name'];
+]
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://allfont.net/allfont.css?fonts=pricedown" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="style.css">
+<!-- 1. Sudarykite masyvą, kuriame būtų aprašyta Vilniaus:
+  "mayor" meras, string
+  "address" savivaldybės adresas, string
+  "number" numeris, string
+  "area" plotas kvadratiniais kilometrais, int
+  "elderships" seniunijų masyvas [] - BENT 3 SENIŪNIJOS -->
+<!-- 2. Kiekviena seniunija turi turėti:
+    title: pavadinimą, string
+    elder: seniunas, string
+    population: gyventojų skaičių, number -->
 
-</head>
-<body>
-<div class="bg">
-    <div class="hud">
-        <div>
-            <div>
-                <div class="weapon <?= $active_weapon; ?>"></div>
-            </div>
-            <div class="time-armor-col">
-                <div><?= $game['hud']['time'] ?></div>
-                <div class="armor-bar">
-                    <div class="armor-bar-line" style="width: <?= $game['hud']['armor'] ?>%"></div>
-                </div>
-            </div>
-        </div>
-        <div>
-            <div class="health-bar">
-                <div class="health-bar-line" style="width: <?= $game['hud']['health'] ?>%"></div>
-            </div>
-            <div class="money"><?= $game['hud']['money']; ?></div>
-            <div class="star-block">
-                <?php for ($i = 1; $i <= 6; $i++) : ?>
-                    <span class="<?= $game['hud']['wanted'] < $i ? 'star-empty' : 'star'; ?>"></span>
-                <?php endfor; ?>
-            </div>
-        </div>
-    </div>
-    <?php foreach ($game['objects'] as $object): ?>
-        <div class="object <?php print $object['class']; ?>"
-             style=" left:<?= $object['x']; ?>% ; top: <?= $object['y']; ?>vh">>
-        </div>
-    <?php endforeach; ?>
-</body>
-</html>
+<!-- 3. Atspausdinkite visų seniunijų pavadinimus -->
+
+<!-- 4. Atspaudinkite vienos iš seniunijų duomenis -->
+<?php
+//foreach ($vilnius["elderships"][0] as $info) {
+//    print $info;
+//}
+?>
+<!-- 5. Atspausdinkite visų seniunijų duomenis lentele -->
+<!--<table>-->
+<!--    <thead>-->
+<!--        <tr>-->
+<!--            <td>title</td>-->
+<!--            <td>adress</td>-->
+<!--            <td>elder</td>-->
+<!--            <td>population</td>-->
+<!--        </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($vilnius["elderships"] as $eldership) : ?><!-- {-->
+<!--        <tr>-->
+<!--            <td>--><? //= $eldership['title']; ?><!--</td>-->
+<!--            <td>--><? //= $eldership['address']; ?><!--</td>-->
+<!--            <td>--><? //= $eldership['elder']; ?><!--</td>-->
+<!--            <td>--><? //= $eldership['population']; ?><!--</td>-->
+<!--        </tr>-->
+<!--    --><?php //endforeach ?>
+<!--    </tbody>-->
+<!--</table>-->
+<!-- 6. Atspausdinkite tik tų seniunijų duomenis kur gyventojų kiekis yra iki 20 tūkst. -->
+<?php //foreach ($vilnius["elderships"] as $eldership) {
+//    if ($eldership['population'] > 20000) {
+//        $eldership['title'];
+//        $eldership['address'];
+//        $eldership['elder'];
+//        $eldership['population'];
+//    }
+//
+//}
+//?>
+<!---->
+<!--<table>-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <td>title</td>-->
+<!--        <td>adress</td>-->
+<!--        <td>elder</td>-->
+<!--        <td>population</td>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($vilnius["elderships"] as $eldership) : ?><!-- {-->
+<!--        --><?php //if ($eldership['population'] > 20000) : ?>
+<!--            <tr>-->
+<!--                <td>--><? //= $eldership['title']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['address']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['elder']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['population']; ?><!--</td>-->
+<!--            </tr>-->
+<!--        --><?php //endif; ?>
+<!--    --><?php //endforeach; ?>
+<!--    </tbody>-->
+<!--</table>-->
+<!-- 7. Atspausdinkite tik tų seniunijų duomenis kur gyventojų kiekis yra virš 50 tūkst. -->
+
+<!--<table>-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <td>title</td>-->
+<!--        <td>adress</td>-->
+<!--        <td>elder</td>-->
+<!--        <td>population</td>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($vilnius["elderships"] as $eldership) : ?>
+<!--        --><?php //if ($eldership['population'] > 50000) : ?>
+<!--            <tr>-->
+<!--                <td>--><? //= $eldership['title']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['address']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['elder']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['population']; ?><!--</td>-->
+<!--            </tr>-->
+<!--        --><?php //endif; ?>
+<!--    --><?php //endforeach; ?>
+<!--    </tbody>-->
+<!--</table>-->
+
+<!-- 8. Atspausdinkite tik tų seniunijų duomenis
+   kur gyventojų kiekis yra nuo 30 iki 40 tūktstančių -->
+<!--<table>-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <td>title</td>-->
+<!--        <td>adress</td>-->
+<!--        <td>elder</td>-->
+<!--        <td>population</td>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($vilnius["elderships"] as $eldership) : ?>
+<!--        --><?php //if ($eldership['population'] > 30000 && $eldership['population'] < 40000) : ?>
+<!--            <tr>-->
+<!--                <td>--><? //= $eldership['title']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['address']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['elder']; ?><!--</td>-->
+<!--                <td>--><? //= $eldership['population']; ?><!--</td>-->
+<!--            </tr>-->
+<!--        --><?php //endif; ?>
+<!--    --><?php //endforeach; ?>
+<!--    </tbody>-->
+<!--</table>-->
+<!-- 9. Stulpeliu atspausdinkite kiekvienos seniūnijos gyventojų skaičių -->
+<!--<table>-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <td>population</td>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($vilnius["elderships"] as $eldership) : ?>
+<!--            <tr>-->
+<!--                <td>--><? //= $eldership['population']; ?><!--</td>-->
+<!--            </tr>-->
+<!--    --><?php //endforeach; ?>
+<!--    </tbody>-->
+<!--</table>-->
+<!-- 10. Stulpeliu atspausdinkite kiekvienos seniūnijos gyventojų skaičių, išrikiavus mažėjimo tvarka -->
+<?php
+// Sort the multidimensional array
+//usort($vilnius["elderships"],"custom_sort");
+//// Define the custom sort function
+//function custom_sort($a,$b) {
+//    return $a['population']<$b['population'];
+//}
+//?>
+<!--<table>-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <td>population</td>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($vilnius["elderships"] as $eldership) : ?>
+<!--        <tr>-->
+<!--            <td>--><? //= $eldership['population']; ?><!--</td>-->
+<!--        </tr>-->
+<!--    --><?php //endforeach; ?>
+<!--    </tbody>-->
+<!--</table>-->
+<!-- 11. Susumuokite visų seniūnijos gyventojų skaičių -->
+<?php
+//$sum = 0;
+//    foreach ($vilnius["elderships"] as $eldership) {
+//        $sum += $eldership['population'];
+//    }
+//    print $sum;
+//?>
+<!-- 12. Suskaičiuokite vidutinį gyventojų skaičių vienoje seniūnijoje -->
+<?php
+//usort($vilnius["elderships"], "custom_sort");
+////// Define the custom sort function
+//function custom_sort($a, $b)
+//{
+//    return $a['population'] < $b['population'];
+//}
+//
+////?>
+<?php
+//$sum = 0;
+//foreach ($vilnius["elderships"] as $eldership) {
+//    $sum += $eldership['population'];
+//}
+//print $sum / count($vilnius["elderships"]);
+//
+//?>
+<!-- 13. Suskaičiuokite vidutinį gyventojų skaičių vienoje seniūnijoje atmetus didžiausią ir mažiausią seniuniją -->
+<?php
+usort($vilnius["elderships"], "custom_sort");
+// Define the custom sort function
+function custom_sort($a, $b)
+{
+    return $a['population'] < $b['population'];
+}
+
+//?>
