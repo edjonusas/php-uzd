902e1107619eaec520821358ab2a405c6cfecdcc
Edwis0513 <ed.jonusas@gmail.com>
2020-08-26 12:09:00 +0300

    inputai

--- index.php
+++ index.php
@@ -1,103 +1,39 @@
 <?php
-$vilnius = [
-    "mayor" => 'Remigijus Šimašius',
-    "number" => '+370 35 654 84',
-    "area" => 401,
-    "elderships" => [
-        [
-            "title" => "Antakalnio seniūnija",
-            "address" => "Antakalnio g. 17, 10312 Vilnius",
-            "elder" => "Taurintas Rudys",
-            "population" => 52369
-        ],
-        [
-            "title" => "Fabijoniškių",
-            "address" => "S. Stanevičiaus g. 24, LT-07102 Vilnius",
-            "elder" => "Jonas Novikevičius",
-            "population" => 22369
-        ],
-        [
-            "title" => "Grigiškių",
-            "address" => "Vilniaus g. 6, LT-27101 Grigiškės, Vilniaus m. sav.",
-            "elder" => "Leonard Klimovič",
-            "population" => 32111
-        ],
-        [
-            "title" => "Naujininkų Seniūnija",
-            "address" => "Dariaus ir Girėno g. 11, LT-02170 Vilnius",
-            "elder" => "Raimondas Lingys",
-            "population" => 37001
-        ],
-        [
-            "title" => "Senamiesčio seniūnija",
-            "address" => "Odminių g. 3, LT-01122 Vilnius",
-            "elder" => "Irena Paukštytė",
-            "population" => 19423
-        ],
-    ]
-];
 
-//function print_array($array)
-//{
-//    $string = '';
-//    foreach ($array as $key => $value) {
-//        gettype($key) === 'string' ? $string .= $key . ' : ' : '';
-//        gettype($value) === 'string' ? $string .= $value . ', ' : '';
-//        foreach ($value as $k => $v) {
-//            gettype($k) === 'string' ? $string .= $k . ' : ' : '';
-//            gettype($v) === 'string' ? $string .= $v . ', ' : '';
-//        }
-//    }
-//    return $string;
-//}
-//
-//var_dump(print_array($vilnius));
+declare(strict_types=1);
 
-//function array_keys_val(array $array)
-//{
-//    $arr = [];
-//
-//    foreach ($array as $key => $value) {
-//        $arr[] = $key;
-//        $arr[] = $value;
-//        if (is_array($value)) {
-//            $arr = array_merge($arr, array_keys_val($value));
-//        }
-//    }
-//
-//    return $arr;
-//}
-//
-//var_dump(array_keys_val($vilnius));
-
- //matrix
-function generate_matrix($num1, $num2)
+/**
+ *function to square number
+ * @param int $number
+ * @return int
+ */
+function square(int $number): int
 {
-    $arr = [];
-    $arr_row = [];
-    for ($i = 0; $i < $num1; $i++) {
-        for ($x = 0; $x < $num2; $x++) {
-            $arr_row[] = rand(0, 1);
-        }
-        $arr[$i] = $arr_row;
-        $arr_row = [];
-    }
-    return $arr;
+    return pow($number, 2);
 }
 
-$matrix = generate_matrix(5, 3);
-var_dump($matrix);
+if (isset($_POST['number']) && is_numeric(trim($_POST['number']))) {
+    $text = square(intval(trim($_POST['number'])));
+} else {
+    $text = 'enter number';
+}
+?>
 
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
 
-//$array = ['b', 'a', 'b', 'e', 'a', 'i'];
-//
-//function replace_values(&$array, $from, $to)
-//{
-//    foreach ($array as $key => $item) {
-//        $item === $from ? $array[$key] = $to : '';
-//    }
-//}
-//
-//replace_values($array, 'b', 'cool');
-//
-//var_dump($array);
+<body>
+<form method="POST">
+	<input name="number" type="text">
+	<button name="submit" value="submit">Submit</button>
+</form>
+<div><?= $text ?></div>
+</body>
+</html>
