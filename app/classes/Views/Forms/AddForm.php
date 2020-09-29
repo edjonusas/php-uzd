8457357016daa2867a5dc8bc2deaaad521bf8671
Edwis0513 <ed.jonusas@gmail.com>
2020-09-29 11:27:14 +0300

    fixes

--- app/classes/Views/Forms/AddForm.php
+++ app/classes/Views/Forms/AddForm.php
@@ -6,101 +6,102 @@ use Core\Views\Form;
 
 class AddForm extends Form
 {
-    private array $form = [
-        'attr' => [
-            'method' => 'POST',
-            'id' => 'add-pixel'
-        ],
-        'fields' => [
-            'x' => [
-                'type' => 'text',
-                'value' => '',
-                'validators' =>
-                    [
-                        'validate_field_not_empty',
-                        'validate_field_is_number',
-                        'validate_field_range' => [
-                            'min' => 0,
-                            'max' => 499,
-                        ],
-                    ],
-                'extra' => [
-                    'attr' => [
-                        'placeholder' => 'Enter X coordinates'
-                    ]
-                ]
-            ],
-            'y' => [
-                'type' => 'text',
-                'value' => '',
-                'validators' =>
-                    [
-                        'validate_field_not_empty',
-                        'validate_field_is_number',
-                        'validate_field_range' => [
-                            'min' => 0,
-                            'max' => 499,
-                        ],
-                    ],
-                'extra' => [
-                    'attr' => [
-                        'placeholder' => 'Enter Y coordinates'
-                    ]
-                ]
-            ],
 
-            'colour' => [
-                'type' => 'select',
-                'value' => 'red',
-                'validators' => [
-                    'validate_selector_value',
-                ],
-                'option' => [
-                    'red' => 'Red',
-                    'blue' => 'Blue',
-                    'green' => 'Green',
-                    'black' => 'Black',
-                    'yellow' => 'Yellow',
-                ],
-                'extra' => [
-                    'attr' => [
-                        'class' => 'colour-selector'
-                    ]
-                ]
-            ],
-            'pixel_size' => [
-                'label' => 'Pixel size',
-                'type' => 'range',
-                'value' => '10',
-                'min_value' => '1',
-                'max_value' => '20',
-                'validators' =>
-                    [
-                        'validate_field_not_empty',
-                        'validate_field_is_number',
-                        'validate_field_range' => [
-                            'min' => 1,
-                            'max' => 20,
-                        ],
-                    ],
-            ],
-        ],
-
-        'buttons' => [
-            'login' => [
-                'title' => 'Add pixel',
-            ],
-        ],
-        'validators' => [
-            'validate_pixel_unique_position' => [
-                'x',
-                'y'
-            ],
-        ],
-    ];
-
-    public function __construct(array $form = [])
+    public function __construct()
     {
-        parent::__construct($this->form);
+        $form = [
+            'attr' => [
+                'method' => 'POST',
+                'id' => 'add-pixel'
+            ],
+            'fields' => [
+                'x' => [
+                    'type' => 'text',
+                    'value' => '',
+                    'validators' =>
+                        [
+                            'validate_field_not_empty',
+                            'validate_field_is_number',
+                            'validate_field_range' => [
+                                'min' => 0,
+                                'max' => 499,
+                            ],
+                        ],
+                    'extra' => [
+                        'attr' => [
+                            'placeholder' => 'Enter X coordinates'
+                        ]
+                    ]
+                ],
+                'y' => [
+                    'type' => 'text',
+                    'value' => '',
+                    'validators' =>
+                        [
+                            'validate_field_not_empty',
+                            'validate_field_is_number',
+                            'validate_field_range' => [
+                                'min' => 0,
+                                'max' => 499,
+                            ],
+                        ],
+                    'extra' => [
+                        'attr' => [
+                            'placeholder' => 'Enter Y coordinates'
+                        ]
+                    ]
+                ],
+
+                'colour' => [
+                    'type' => 'select',
+                    'value' => 'red',
+                    'validators' => [
+                        'validate_selector_value',
+                    ],
+                    'option' => [
+                        'red' => 'Red',
+                        'blue' => 'Blue',
+                        'green' => 'Green',
+                        'black' => 'Black',
+                        'yellow' => 'Yellow',
+                    ],
+                    'extra' => [
+                        'attr' => [
+                            'class' => 'colour-selector'
+                        ]
+                    ]
+                ],
+                'pixel_size' => [
+                    'label' => 'Pixel size',
+                    'type' => 'range',
+                    'value' => '10',
+                    'min_value' => '1',
+                    'max_value' => '20',
+                    'validators' =>
+                        [
+                            'validate_field_not_empty',
+                            'validate_field_is_number',
+                            'validate_field_range' => [
+                                'min' => 1,
+                                'max' => 20,
+                            ],
+                        ],
+                ],
+            ],
+
+            'buttons' => [
+                'login' => [
+                    'title' => 'Add pixel',
+                ],
+            ],
+            'validators' => [
+                'validate_pixel_unique_position' => [
+                    'x',
+                    'y'
+                ],
+            ],
+        ];
+
+        parent::__construct($form);
     }
 }
--- app/classes/Views/Forms/RegisterForm.php
+++ app/classes/Views/Forms/RegisterForm.php
@@ -6,59 +6,60 @@ use Core\Views\Form;
 
 class RegisterForm extends Form
 {
-    private array $form = [
-        'attr' => [
-            'method' => 'POST',
-            'id' => 'register'
-        ],
-        'fields' => [
-            'user_name' => [
-                'label' => 'User name',
-                'type' => 'text',
-                'value' => '',
-                'validators' =>
-                    [
-                        'validate_field_not_empty',
-                        'validate_user_unique',
-                    ],
-                'extra' => [
-                    'attr' => [
-                        'placeholder' => 'Enter Your User name'
-                    ]
-                ]
-            ],
-            'password' => [
-                'label' => 'Password',
-                'type' => 'password',
-                'validators' =>
-                    [
-                        'validate_field_not_empty',
-                    ],
-            ],
-            'password_repeat' => [
-                'label' => 'Repeat Password',
-                'type' => 'password',
-                'validators' =>
-                    [
-                        'validate_field_not_empty',
-                    ],
-            ],
-        ],
-        'buttons' => [
-            'register' => [
-                'title' => 'Register',
-            ],
-        ],
-        'validators' => [
-            'validate_fields_match' => [
-                'password',
-                'password_repeat'
-            ],
-        ],
-    ];
 
-    public function __construct(array $form = [])
+    public function __construct()
     {
-        parent::__construct($this->form);
+        $form = [
+            'attr' => [
+                'method' => 'POST',
+                'id' => 'register'
+            ],
+            'fields' => [
+                'user_name' => [
+                    'label' => 'User name',
+                    'type' => 'text',
+                    'value' => '',
+                    'validators' =>
+                        [
+                            'validate_field_not_empty',
+                            'validate_user_unique',
+                        ],
+                    'extra' => [
+                        'attr' => [
+                            'placeholder' => 'Enter Your User name'
+                        ]
+                    ]
+                ],
+                'password' => [
+                    'label' => 'Password',
+                    'type' => 'password',
+                    'validators' =>
+                        [
+                            'validate_field_not_empty',
+                        ],
+                ],
+                'password_repeat' => [
+                    'label' => 'Repeat Password',
+                    'type' => 'password',
+                    'validators' =>
+                        [
+                            'validate_field_not_empty',
+                        ],
+                ],
+            ],
+            'buttons' => [
+                'register' => [
+                    'title' => 'Register',
+                ],
+            ],
+            'validators' => [
+                'validate_fields_match' => [
+                    'password',
+                    'password_repeat'
+                ],
+            ],
+        ];
+
+        parent::__construct($form);
     }
 }
